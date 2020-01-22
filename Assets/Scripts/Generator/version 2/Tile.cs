02b611e8db174e11a3c2cf7cb7bdf1936e72ece1
Alex-Trim <55793685+Alex-Trim@users.noreply.github.com>
2020-01-22 20:09:59 +0300

    Create Tile.cs

---
+++ Assets/Scripts/Generator/version 2/Tile.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Transform Begin;
+    public Transform End;
+    public Land[] Lands;
+}
