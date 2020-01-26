07083b0dd08d6d84c52d5a3c9ac3ac3e499b3f51
Alex-Trim <55793685+Alex-Trim@users.noreply.github.com>
2020-01-26 10:13:21 +0300

    Update HitArea.cs

--- Assets/Scripts/Attack/HitArea.cs
+++ Assets/Scripts/Attack/HitArea.cs
@@ -6,9 +6,11 @@ public class HitArea : MonoBehaviour
 {
     public Transform Arrow;
     public Transform[] Enumy;
+   
 
     private int countEnumy;
     private SightScale sightScale;
+   
     void Start()
     {
         sightScale = FindObjectOfType<SightScale>();
@@ -18,19 +20,25 @@ public class HitArea : MonoBehaviour
 
     void Update()
     {
-        if (Input.touchCount>0 || Input.GetMouseButtonDown(0))
-        {
-            if (Arrow.localEulerAngles.z >= transform.localEulerAngles.z && Arrow.localEulerAngles.z <= transform.localEulerAngles.z+18)
+          if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
             {
-                transform.localEulerAngles = new Vector3(0, 0, Random.Range(0f, 68f));
-                if (countEnumy>=0) 
-                { 
-                    Enumy[countEnumy].gameObject.SetActive(false);
-                    --countEnumy; 
-                }
+                if (Arrow.localEulerAngles.z >= transform.localEulerAngles.z && Arrow.localEulerAngles.z <= transform.localEulerAngles.z + 18)
+                {
+                    transform.localEulerAngles = new Vector3(0, 0, Random.Range(0f, 68f));
+                    if (countEnumy >= 0)
+                    {
+                        Enumy[countEnumy].gameObject.SetActive(false);
+                        --countEnumy;
+                    }
+                    if (countEnumy == 0)
+                    {
+                    
+                        sightScale.Stop();
+                    }
+                Debug.Log(countEnumy);
                 sightScale.BafSpeed();
-                Debug.Log("True");
+                }
             }
-        }
+        
     }
 }
