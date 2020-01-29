c2f85088aeb7c6b4c35f1294141e808c5a3c90cc
RevanVI <47805713+RevanVI@users.noreply.github.com>
2020-01-29 15:30:38 +0300

    Merge with attack branch

---
+++ Assets/Scripts/UIMenu/UIStartMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIStartMenu : MonoBehaviour
+{
+    public GameObject ShopMenu;
+    public GameObject StartMenu;
+    public void StartGame()
+    {
+        Debug.Log("LoadScene('play')");
+       // SceneManager.LoadScene("play");
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("OpenSettings");
+    }
+    public void OpenShop()
+    {
+        StartMenu.SetActive(false);
+        ShopMenu.SetActive(true);
+    }
+    public void ClouseShop()
+    {
+        StartMenu.SetActive(true);
+        ShopMenu.SetActive(false);
+    }
+}
