b3e547ed3bdd2e1d7e20798242fd571523ed37cf
Alex-Trim <55793685+Alex-Trim@users.noreply.github.com>
2020-02-06 17:35:03 +0300

    AddStartMenu

---
+++ Assets/Scripts/AudioControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControl : MonoBehaviour
+{
+    AudioSource audio;
+    void Start()
+    {
+        audio = gameObject.GetComponent<AudioSource>();
+        if(PlayerPrefs.GetInt("Audio") == 1)
+        {
+            Debug.Log("true");
+            audio.Play();
+        }
+        else
+        {
+            Debug.Log("FFFF");
+            audio.Stop();
+        }
+    }
+
+}
--- Assets/Scripts/UIMenu/UIStartMenu.cs
+++ Assets/Scripts/UIMenu/UIStartMenu.cs
@@ -7,24 +7,59 @@ public class UIStartMenu : MonoBehaviour
 {
     public GameObject ShopMenu;
     public GameObject StartMenu;
+    public GameObject InfoMenu;
+    public GameObject AudioOff;
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt("Audio") == 1)
+        {
+            AudioOff.SetActive(false);
+        }
+        else
+        {
+            AudioOff.SetActive(true);
+        }
+    }
     public void StartGame()
     {
         Debug.Log("LoadScene('play')");
-       // SceneManager.LoadScene("play");
+        SceneManager.LoadScene("play");
     }
-
-    public void OpenSettings()
+    public void Map()
     {
-        Debug.Log("OpenSettings");
+        SceneManager.LoadScene("Map");
+    }
+    public void OpenInfo()
+    {
+        StartMenu.SetActive(false);
+        InfoMenu.SetActive(true);
+    }
+    public void ClouseInfo()
+    {
+        StartMenu.SetActive(true);
+        InfoMenu.SetActive(false);
+    }
+    public void ClouseShop()
+    {
+        StartMenu.SetActive(true);
+        ShopMenu.SetActive(false);
     }
     public void OpenShop()
     {
         StartMenu.SetActive(false);
         ShopMenu.SetActive(true);
     }
-    public void ClouseShop()
+    public void AudioOffON()
     {
-        StartMenu.SetActive(true);
-        ShopMenu.SetActive(false);
+        if (!AudioOff.active)
+        {
+            AudioOff.SetActive(true);
+            PlayerPrefs.SetInt("Audio", 0);
+        }
+        else 
+        { 
+            AudioOff.SetActive(false);
+            PlayerPrefs.SetInt("Audio", 1);
+        }
     }
 }
