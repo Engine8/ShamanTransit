5401aaa1474131a152f288bd1d9b8c361a68853d
RevanVI <revan0603@gmail.com>
2020-02-07 19:41:10 +0300

    Base save system done

--- Assets/Scripts/PlayerDataController.cs
+++ Assets/Scripts/PlayerDataController.cs
@@ -6,45 +6,70 @@ using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 [Serializable]
-public class PlayerDataController
+public class PlayerData
 {
-    [NonSerialized]
-    public static PlayerDataController Instance;
-
     public int Money;
     public int CurrentStage;
     public int CurrentLevel;
     public List<int> PurchaseIndexes;
 
-    public PlayerDataController(int money, int curStage, int curLevel, List<int> purchaseIndexes)
+    public PlayerData(int money = 500, int curStage = 1, int curLevel = 1, List<int> purchaseIndexes = null)
     {
-        if (Instance == null)
+        Money = money;
+        CurrentStage = curStage;
+        CurrentLevel = curLevel;
+        PurchaseIndexes = purchaseIndexes;
+    }
+
+}
+
+public class PlayerDataController
+{
+    
+    private static PlayerDataController _instance;
+    public PlayerDataController Instance
+    {
+        get
         {
-            Instance = this;
+            return _instance;
         }
-        Instance.Money = money;
-        Instance.CurrentStage = curStage;
-        Instance.CurrentLevel = curLevel;
-        Instance.PurchaseIndexes = purchaseIndexes;
+    }
+
+    public PlayerData Data;
+
+    private string _filePath = Application.persistentDataPath + "/data.dat";
+
+    public static void Initialize()
+    {
+        if (_instance == null)
+            _instance = new PlayerDataController();
+    }
+
+    private PlayerDataController()
+    {
+        if (!File.Exists(_filePath))
+            Data = new PlayerData();
+        else
+            LoadData();
     }
 
     public void AddMoney(int value)
     {
-        Money += value;
+        Data.Money += value;
     }
 
     public void ItemPurchased(int itemIndex)
     {
-        PurchaseIndexes.Add(itemIndex);
+        Data.PurchaseIndexes.Add(itemIndex);
     }
 
     public void WriteData()
     {
         BinaryFormatter formatter = new BinaryFormatter();
         // получаем поток, куда будем записывать сериализованный объект
-        using (FileStream fs = new FileStream(Application.persistentDataPath + "/data.dat", FileMode.OpenOrCreate))
+        using (FileStream fs = new FileStream(_filePath, FileMode.OpenOrCreate))
         {
-            formatter.Serialize(fs, this);
+            formatter.Serialize(fs, Data);
             Debug.Log("Save was written");
         }
     }
@@ -52,9 +77,9 @@ public class PlayerDataController
     public void LoadData()
     {
         BinaryFormatter formatter = new BinaryFormatter();
-        using (FileStream fs = new FileStream(Application.persistentDataPath + "/data.dat", FileMode.OpenOrCreate))
+        using (FileStream fs = new FileStream(_filePath, FileMode.Open))
         {
-            PlayerDataController newData = (PlayerDataController)formatter.Deserialize(fs);
+            Data = (PlayerData)formatter.Deserialize(fs);
             Debug.Log("Save was loaded");
         }
     }
