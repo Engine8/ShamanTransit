fc6f9aac7e99fd1872e8acd16dd43e8fe84934eb
RevanVI <47805713+RevanVI@users.noreply.github.com>
2020-02-10 21:20:54 +0300

    Done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : Movable
-{
-    public GameObject[] Lines;
-    
+{    
     private SpriteRenderer _spriteRenderer;
     private ParticleSystem _cargoParticle;
 
@@ -28,38 +26,59 @@ public class PlayerController : Movable
             }
         });
         base.Start();
-
     }
 
     private void Update()
     {
-        if (Input.GetButtonDown("Up"))
+        //change line
+        if (Input.GetButtonDown("Up") && !_isLineSwapBlocked)
         {
             gameObject.layer -= 1;
+            _targetLine -= 1;
             if (gameObject.layer < 8)
                 gameObject.layer = 8;
+            if (_targetLine < 0)
+                _targetLine = 0;
         }
-        else if (Input.GetButtonDown("Down"))
+        else if (Input.GetButtonDown("Down") && !_isLineSwapBlocked)
         {
             gameObject.layer += 1;
+            _targetLine += 1;
             if (gameObject.layer > 10)
                 gameObject.layer = 10;
+            if (_targetLine > 2)
+                _targetLine = 2;
         }
-        if (gameObject.layer == 8)
+
+        //define sorting layer
+        if (_targetLine > _curLine)
         {
-            _spriteRenderer.sortingLayerName = "Line1";
-        }
-        else if (gameObject.layer == 9)
-        {
-            _spriteRenderer.sortingLayerName = "Line2";
+            if (_targetLine == 1)
+            {
+                _spriteRenderer.sortingLayerName = "Line2";
+            }
+            else
+            {
+                _spriteRenderer.sortingLayerName = "Line3";
+            }
         }
         else
         {
-            _spriteRenderer.sortingLayerName = "Line3";
+            if (_curLine == 0)
+            {
+                _spriteRenderer.sortingLayerName = "Line1";
+            }
+            else if (_curLine == 1)
+            {
+                _spriteRenderer.sortingLayerName = "Line2";
+            }
+            else
+            {
+                _spriteRenderer.sortingLayerName = "Line3";
+            }
         }
-
     }
-
+    /*
     new void FixedUpdate()
     {
         //very bad things: need find method to get layer name
@@ -78,5 +97,5 @@ public class PlayerController : Movable
         }
         base.FixedUpdate();
     }
-
+    */
 }
