a64bf432b81c8dfb218e61685407da7b8948f2e4
Alex-Trim <55793685+Alex-Trim@users.noreply.github.com>
2020-03-03 12:11:25 +0300

    levl5

--- Assets/Scripts/Attack/Tach.cs
+++ Assets/Scripts/Attack/Tach.cs
@@ -7,6 +7,11 @@ using UnityEngine.EventSystems;
 public class Tach : MonoBehaviour, IPointerDownHandler
 {
     public HitArea hit;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            hit.Tach();
+    }
     public virtual void OnPointerDown(PointerEventData ped)
     {
         if(FindObjectOfType<HitArea>())
