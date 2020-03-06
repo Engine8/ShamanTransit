50a21f4dbf2faffde8c6c4487f53b0e8d0a73b03
RevanVI <revan0603@gmail.com>
2020-03-06 17:30:44 +0300

    step 1

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    private AudioSource _musicSource;
+    private List<AudioSource> _soundSources;
+
+
+    private static SoundManager _instance;
+
+    public static SoundManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    public static void Initialize()
+    {
+        if (_instance != null)
+            return;
+        
+        GameObject soundManagerGameObject = new GameObject("SoundManager");
+        _instance = soundManagerGameObject.AddComponent<SoundManager>();
+        DontDestroyOnLoad(soundManagerGameObject);
+    }
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayMusic(AudioClip music)
+    {
+        //stop current music 
+
+        if (_musicSource == null)
+        {
+            
+            _musicSource = new AudioSource();
+            //setup new audio surce
+        }
+
+        _musicSource.clip = music;
+        _musicSource.Play();
+    }
+
+}
