fa9e99dd3f5c7ed0bbef71df0b06d9427e6bccb8
Alex-Trim <55793685+Alex-Trim@users.noreply.github.com>
2020-03-23 21:47:09 +0300

    UI

---
+++ Assets/Scripts/Helper/HelpControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HelpControl : MonoBehaviour
+{
+    public static HelpControl helpControl;
+    private HelpUI _helpUI;
+    private void Awake()
+    {
+        if (helpControl == null)
+        {
+            helpControl = this;
+        }
+    }
+    public void Help(GameObject MessageUI)
+    {
+        Debug.Log("Help " + MessageUI.name);
+        _helpUI = new HelpUI(MessageUI);
+        StartCoroutine(AwakeMessage());
+    }
+    public void Move()
+    {
+        Time.timeScale = 1f;
+        _helpUI.setActiv(false);
+    }
+    IEnumerator AwakeMessage()
+    {
+        _helpUI.setActiv(0, true);
+        Time.timeScale = 0f;
+            yield return new WaitForSecondsRealtime(3f);
+        _helpUI.setActiv(0, false);
+        _helpUI.setActiv(1, true);
+    }
+    class HelpUI
+    {
+        GameObject[] MessageUI =new GameObject[2];
+        public HelpUI(GameObject helpUI)
+        {
+            if (helpUI.transform.childCount == 2)
+            {
+                MessageUI[0] = helpUI.transform.GetChild(0).gameObject;
+                MessageUI[1] = helpUI.transform.GetChild(1).gameObject;
+            }
+            else
+            {
+                MessageUI[0] = helpUI.transform.GetChild(0).gameObject;
+                MessageUI[1] = null;
+            }
+        }
+        public void setActiv( bool activ)
+        {
+            MessageUI[0].SetActive(activ);
+            MessageUI[1].SetActive(activ);
+        }
+        public void setActiv(int index, bool activ)
+        {
+            if(MessageUI[index])
+                MessageUI[index].SetActive(activ);
+        }
+    }
+}
---
+++ Assets/Scripts/Helper/TriggerHelp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class TriggerHelp : MonoBehaviour
+{
+    public GameObject MessageUI;
+    public delegate void MessageStartHelp(GameObject ingex);
+    public event MessageStartHelp message;
+
+    private void Start()
+    {
+        if(HelpControl.helpControl!=null)
+            message += HelpControl.helpControl.Help;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        message.Invoke(MessageUI);
+    }
+}
