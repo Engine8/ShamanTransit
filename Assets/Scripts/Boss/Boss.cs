4cda26cd0df0ad4a8a73e3317d5b39688fb0013b
Alex-Trim <55793685+Alex-Trim@users.noreply.github.com>
2020-05-12 23:54:53 +0300

    предупреждения

--- Assets/Scripts/Boss/Boss.cs
+++ Assets/Scripts/Boss/Boss.cs
@@ -4,21 +4,28 @@ using Unity.UIWidgets.ui;
 using UnityEngine;
 
 public class Boss : EnemyController
-{  
-    enum LineAttack {Line1= -127,Line2=-206,Line3=-299 };
-
+{
+    float[]  LineAttack = {-1.8f, -2.52f, -3.62f };
+    float[,]  LineSize = { {4f, 1.5f }, {4.5f,2f},{ 5f,2.2f} };
+    string[] LayerName = { "Line1", "Line2", "Line3" };
+    GameObject[] AttackWarning;
     public int helf;
 
     private float _speedBuf;
+    public float timeBetweenAttacks;//скорость атаки
 
     private PlayerController Player;
-    private float nextAttackTime;
+    private float nextAttackTime=10;
     private bool _canAttack = true;
     private bool Dead = false;
     //Destoyed in HitArea class
 
     void Start()
     {
+        AttackWarning = new GameObject[transform.childCount];
+        for (int i=0;i< transform.childCount; i++)
+            AttackWarning[i] = transform.GetChild(i).gameObject;
+
         Player = FindObjectOfType<PlayerController>().GetComponent<PlayerController>();
         transform.position = new Vector2(Player.transform.position.x - 13, 0);
 
@@ -37,7 +44,26 @@ public class Boss : EnemyController
         if (!Dead && !_isInAnimation)
         {
             gameObject.transform.localPosition = new Vector2(gameObject.transform.localPosition.x +( Player.Speed+_speedBuf) * Time.deltaTime, gameObject.transform.localPosition.y);
+            if (!Player.GetDead())
+            {
+                if (Time.time > nextAttackTime) 
+                {
+                    StartCoroutine("Warning");
+                    nextAttackTime = Time.time + timeBetweenAttacks;
+                }
+            }
         }
+    }
+    IEnumerator Warning() //появление волков
+    {
+        int value = Random.Range(0, 2);
+        int position = Random.Range(0, 3);
+        AttackWarning[value].SetActive(true);
+        AttackWarning[value].transform.position = new Vector2(AttackWarning[value].transform.position.x, LineAttack[position]);
+        AttackWarning[value].transform.localScale = new Vector2(AttackWarning[value].transform.localScale.x , LineSize[position, value]);
+        AttackWarning[value].GetComponent<SpriteRenderer>().sortingLayerName = LayerName[position];
+        yield return new WaitForSeconds(2f);  
+        AttackWarning[value].SetActive(false);
     }
     public override void TakeDamage()
     {
